8fbb65d2f9bc6a39f08e14da77b3b59faf6bffc9
Chanuka <it21281082@my.sliit.lk>
2025-02-13 00:42:39 +0530

    Keys updated

--- firebase/index.js
+++ firebase/index.js
@@ -7,12 +7,12 @@ import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, deleteDoc   
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB-GKYVJ9LvTWO39ubv_AdlHI8w_YCs3QI",
-  authDomain: "shopping-app-7df11.firebaseapp.com",
-  projectId: "shopping-app-7df11",
-  storageBucket: "shopping-app-7df11.firebasestorage.app",
-  messagingSenderId: "349325953625",
-  appId: "1:349325953625:web:24d773b9290da063817f48"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
